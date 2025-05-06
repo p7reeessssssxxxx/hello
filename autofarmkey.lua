675ac00e6857d4949d692aa1d65804bdae338c3a
p7reeessssssxxxx <ttvn3006@gmail.com>
2025-05-06 12:22:54 +0100

    Create autofarmkey.lua

---
+++ autofarmkey.lua
@@ -0,0 +1,49 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "Yapppppp",
+    Icon = 0, 
+    LoadingTitle = "deadrails autofarm keysystem",
+    LoadingSubtitle = "autofarm bounds",
+    Theme = "Default", 
+ 
+    DisableRayfieldPrompts = false,
+    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+ 
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = BluHub, -- Create a custom folder for your hub/game
+       FileName = "BluHub"
+    },
+ 
+    Discord = {
+       Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+       Invite = "https://discord.gg/pcabxYSBSJ", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+    },
+ 
+    KeySystem = true,
+    KeySettings = {
+       Title = "Autofarm - deadrails",
+       Subtitle = "Key System",
+       Note = "Join the discord for the key https://discord.gg/tmSTMS78MZ", -- Use this to tell the user how to get a key
+       FileName = "Yapuh", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       Key = {"keyapple1234"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+    }
+ })
+
+ local Tab = Window:CreateTab("Autofarm - deadrails", 4483362458) -- Title, Image
+
+ local Section = Tab:CreateSection("tap to load script")
+ Section:Set("Section Example")
+ 
+ local Button = Tab:CreateButton({
+    Name = "Tap to load script",
+    Callback = function()
+        loadstring(game:HttpGet("https://rawscripts.net/raw/Dead-Rails-Alpha-Autobonds-NO-KEY-38587"))() 
+    end,
+ })
+
+ Rayfield:LoadConfiguration()
