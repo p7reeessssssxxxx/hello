ecf835f67e7b9c8e3fec29a1e49c18eaf2cd3422
p7reeessssssxxxx <203464561+p7reeessssssxxxx@users.noreply.github.com>
2025-11-07 19:17:34 +0000

    Add MoonSec protection and print loop

---
+++ 3333.lua
@@ -0,0 +1,15 @@
+MoonSec_StringsHiddenAttr = true;
+if __gkaCMtPHWOT ~= "This file was protected with MoonSec V3" then
+    return;
+else
+    local v0 = {};
+    for v1 = 333, 999 do
+        v0[{}] = v1;
+        v0[v1] = v1;
+    end;
+    for _, v3 in v0 do
+        print(v3);
+        return;
+    end;
+    return;
+end;
